79726c48d6f4eb97374068ef57f2ea6c70bbaa0c
HuamouChen <chenhm4444@gmail.com>
2019-06-09 16:44:59 +0800

    first commit

---
+++ Classes/CHMLogTool.h
@@ -0,0 +1,17 @@
+//
+//  CHMLogTool.h
+//  MyInput
+//
+//  Created by hm on 2019/6/9.
+//  Copyright © 2019 hm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CHMLogTool : NSObject
++ (void)chm_logWithString:(NSString *)string;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/CHMLogTool.m
@@ -0,0 +1,15 @@
+//
+//  CHMLogTool.m
+//  MyInput
+//
+//  Created by hm on 2019/6/9.
+//  Copyright © 2019 hm. All rights reserved.
+//
+
+#import "CHMLogTool.h"
+
+@implementation CHMLogTool
++ (void)chm_logWithString:(NSString *)string {
+    NSLog(@"This is log: %@", string);
+}
+@end
